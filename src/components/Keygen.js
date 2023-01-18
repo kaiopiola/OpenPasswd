6fbcfc0834e24f08c5ab4efa598a45ecd4183983
Kaio Piola <35163593+kaiopiola@users.noreply.github.com>
2023-01-18 03:29:22 +0000

    improve(keygen): manually sets pass preferences

--- src/components/Keygen.js
+++ src/components/Keygen.js
@@ -1,31 +1,71 @@
 import React, { useState } from 'react';
 
-function randomizer(pattern, size) {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789!@#$%&*()_;.,?:{^}[~]";
+function randomizer(size, uppercase, lowercase, special, numbers) {
+    let characters = '';
+    if(uppercase==true)
+    {
+        characters += 'ABCDEFGHIJKLMNOPQRSTUVXWYZ';
+    }
+    if(lowercase==true)
+    {
+        characters += 'abcdefghijklmnopqrstuvxwyz';
+    }
+    if(numbers==true)
+    {
+        characters += '0123456789';
+    }
+    if(special==true)
+    {
+        characters += '!@#$%&*()_;.,?:{^}[~]';
+    }
+    
+
+    // const characters = "ABCDEFGHIJKLMNOPQRSTUVXWYZ0123456789!@#$%&*()_;.,?:{^}[~]";
     let result = "";
     for (var i = 0; i < size; i++) {
-            result += characters.charAt(Math.floor(Math.random() * characters.length));
-      }
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
     return result;
 }
 
 function Keygen() {
     const [result, setResult] = useState();
     const [size, setSize] = useState(12);
-    let pattern = "XXXXXXXX";
+    const [uppercase, setUppercase] = useState(true);
+    const [lowercase, setLowercase] = useState(true);
+    const [special, setSpecial] = useState(true);
+    const [numbers, setNumbers] = useState(true);
 
     return (
         <>
 
-        <input type="text" value={result} />
-            <button onClick={() => setResult(randomizer(pattern, size))}>Generate new pass</button>
+            <input type="text" value={result} />
+            <button onClick={() => setResult(randomizer(size, uppercase, lowercase, special, numbers))}>Generate new pass</button>
 
             <div>
-    <h3>Options</h3>
-    <input type="range" min="8" max="26" defaultValue='12' onChange={(e) => setSize(e.target.value)} /> <span>{size}</span>
-    <br/>
-    <input type="checkbox" id='' />
-</div>
+                <h3>Options</h3>
+                <div>
+                    <label>Password length</label>
+                    <input type="range" min="8" max="26" defaultValue='12' onChange={(e) => setSize(e.target.value)} /> <span>{size}</span>
+                </div>
+
+                <div>
+                    <label>Uppercase</label>
+                    <input type="checkbox" defaultChecked onChange={(e) => setUppercase(e.target.checked)} />
+                </div>
+                <div>
+                    <label>Lowercase</label>
+                    <input type="checkbox" defaultChecked onChange={(e) => setLowercase(e.target.checked)} />
+                </div>
+                <div>
+                    <label>Numbers</label>
+                    <input type="checkbox" defaultChecked onChange={(e) => setNumbers(e.target.checked)} />
+                </div>
+                <div>
+                    <label>Special characters</label>
+                    <input type="checkbox" defaultChecked onChange={(e) => setSpecial(e.target.checked)} />
+                </div>
+            </div>
         </>
     )
 }
